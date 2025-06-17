e68b72967c515e900ea2c9f252dc76a8385dc991
GanidhuAbey <ganidhu.abey@gmail.com>
2025-06-17 11:29:45 -0700

    fixed issue with marker_types being incorrect size

--- src_python/h5.py
+++ src_python/h5.py
@@ -1,6 +1,5 @@
 import os
 import h5py
-import trimesh
 import numpy as np
 from typing import Dict, List, Any
 
@@ -12,13 +11,9 @@ def set_marker_types(jsons: List[Dict[str, Any]]):
         # TODO: as it stands, springtime is only expecting 1 set of marker types
         #       per rollout, so we will only extract the marker types for the first mesh.
         kinematic = config['handles'][0]['nodes']
-        mesh_path = config['cloths'][0]["mesh"]
-        mesh = trimesh.load_mesh(mesh_path)
-
-        vertex_count = len(mesh.vertices)
-
-        marker_types = np.ones((vertex_count, 1))
-        marker_types[kinematic] = 0
 
         with h5py.File(h5_path, 'a') as f:
+            xpos_particles = f['positions'][:]
+            marker_types = np.ones((xpos_particles.shape[1]))
+            marker_types[kinematic] = 0
             f.create_dataset('marker_types', data=marker_types) 
